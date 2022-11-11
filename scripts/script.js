7c6680ba7a079cd2ace8ce80cac7244b87ecd97b
ram <ramithr11@gmail.com>
2022-11-10 16:09:04 -0800

    adds constants for course list and begins work on logic for prompt

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+const ACIT1620 = {
+    code: "ACIT 1620", name:"Fundamental Web Technologies"
+}
+const MATH1310 = {code: "MATH 1310", name:"Technical Math for IT"
+}
+const ACIT1515 = {code: "ACIT 1515", name:"Scripting for IT"
+}
+const courselist = [ACIT1620, MATH1310, ACIT1515]
+
+var coursec = prompt("Please enter a course code")
+
+if (coursec != int || coursec < 1000 || coursec > 9999) {
+    let coursec = prompt("Invalid response. Please enter a course code")
+    }
+
+if (coursec) in courselist{code},
+
+for (var x = 0; x < 9999; i++) {
+    console.log(`Yes, I am taking this course: ${courselist[x]})
+}
